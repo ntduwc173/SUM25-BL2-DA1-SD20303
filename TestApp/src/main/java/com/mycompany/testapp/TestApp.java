2268c75fc61a72ba9fd8369e7002d5a31dff1561
ntduwc173 <chjlll2k6@gmail.com>
2025-07-05 00:38:16 +0700

    init project

---
+++ TestApp/src/main/java/com/mycompany/testapp/TestApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testapp;
+
+/**
+ *
+ * @author USER
+ */
+public class TestApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
